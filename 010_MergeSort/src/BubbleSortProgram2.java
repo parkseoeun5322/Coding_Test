29e016841f7d094f411417a37dacdb2bae4ca21b
seoeun park <78471888+parkseoeun5322@users.noreply.github.com>
2023-04-14 00:10:42 +0900

    병합정렬(2회독)

--- 010_MergeSort/src/BubbleSortProgram2.java
+++ 010_MergeSort/src/BubbleSortProgram2.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 // 백준 온라인저지 2751번 / 교재 128페이지
 
@@ -15,9 +19,56 @@
 // 출력 : 첫째 줄에 Swap 횟수를 출력한다
 
 public class BubbleSortProgram2 {
+	public static int A[];
+	public static int tmp[];
+	public static int result;
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		A = new int[N];
+		tmp = new int[N];
+		result = 0;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
+		}
+		merge_sort(0, N-1);
+		System.out.println(result);
 	}
 
+	public static void merge_sort(int s, int e) {
+		if(e - s < 1) return;
+		int m = (s + e) / 2;
+		merge_sort(s, m);
+		merge_sort(m + 1, e);
+		for (int i = s; i <= e; i++) {
+			tmp[i] = A[i];
+		}
+		int k = s;
+		int index1 = s;
+		int index2 = m + 1;
+		while(index1 <= m && index2 <= e) {
+			if(tmp[index1] > tmp[index2]) {
+				A[k] = tmp[index2];
+				result = result + (index2 - k);
+				k++;
+				index2++;
+			} else {
+				A[k] = tmp[index1];
+				k++;
+				index1++;
+			}
+		}
+		while(index1 <= m) {
+			A[k] = tmp[index1];
+			k++;
+			index1++;
+		}
+		while(index2 <= e) {
+			A[k] = tmp[index2];
+			k++;
+			index2++;
+		}
+	}
 }
