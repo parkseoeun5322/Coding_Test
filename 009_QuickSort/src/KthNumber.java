604d3c0cd3c5886273ff2e66588efac23ad1f4c1
seoeun park <78471888+parkseoeun5322@users.noreply.github.com>
2023-04-13 23:49:06 +0900

    퀵정렬 & 병합정렬(2회독)

--- 009_QuickSort/src/KthNumber.java
+++ 009_QuickSort/src/KthNumber.java
@@ -1,9 +1,93 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 백준 온라인 저지 11004번 / 교재 121페이지
+
+// 문제 : 수 N개 A1, A2, ..., AN이 주어진다. A를 오름차순 정렬했을 때, 앞에서부터 K번째 있는 수를 구하는 프로그램을 작성하시오.
+
+// 입력 : 첫째 줄에 N(1 ≤ N ≤ 5,000,000)과 K (1 ≤ K ≤ N)이 주어진다.
+//		 둘째에는 A1, A2, ..., AN이 주어진다. (-109 ≤ Ai ≤ 109)
+
+// 출력 : A를 정렬했을 때, 앞에서부터 K번째 있는 수를 출력한다.
 
 public class KthNumber {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		int[] A = new int[N];
+		for (int i = 0; i < N; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
+		}
+		quickSort(A, 0, N-1, K-1);
+		/*
+		for (int i = 0; i < A.length; i++) {
+			System.out.print(A[i] + " | ");
+		}
+		*/
+		System.out.println(A[K-1]);
+	}
+	
+	// 퀵소트 실행하는 함수
+	public static void quickSort(int[] A, int S, int E, int K) {
+		if(S < E) {
+			int pivot = partition(A, S, E);
+			if(pivot == K)
+				return;
+			else if(K < pivot) 
+				quickSort(A, S, pivot - 1, K);
+			else 
+				quickSort(A, pivot + 1, E, K);
+		}
+	}
+	
+	// pivot 구하는 함수
+	public static int partition(int[] A, int S, int E) {
+		if(S + 1 == E) {
+			if(A[S] > A[E]) swap(A, S, E);
+			return E;
+		}
+		int M = (S + E) / 2;
+		swap(A, S, M);			// 중앙값을 첫번째로 swap
+		int pivot = A[S];		// swap한 첫번째 값을 pivot으로 저장
+		int i = S + 1, j = E;	// 첫번째 값을 제외한 나머지 범위 설정
+		while(i <= j) {
+			while(pivot < A[j] && j > 0) {
+				//.out.print("1. pivot ("+pivot+") < A[j] ("+A[j] + ")");
+				j--;
+				//System.out.println(" → j = " + j);
+			}
+			
+			while(pivot > A[i] && i < A.length-1) {
+				//System.out.print("2. pivot ("+pivot+") > A[i] ("+A[i] + ")");
+				i++;
+				//System.out.println(" → i = " + i);
+			}
+			if(i <= j) {
+				// 후위 연산자 주의! swap이 이뤄진 후에 연산된다.
+				swap(A, i++, j--);
+				//System.out.println("3. i = " + i + " / j = " + j);
+			}
+		}
+		//System.out.println("4. A[S] = " + A[S] + " / A[j] = " + A[j] + " / pivot = " + pivot);
+		A[S] = A[j];
+		A[j] = pivot;
+		//System.out.println("5. A[S] = " + A[S] + " / A[j] = " + A[j] + " / pivot = " + pivot);
+		//System.out.println("================= partition 끝");
+		return j;
+	}
+	
+	// swap 함수
+	public static void swap(int[] A, int i, int j) {
+		int temp = A[i];
+		A[i] = A[j];
+		A[j] = temp;
+		//System.out.println("swap =>  A["+i+"] = " + A[i] + " / A["+j+"] = " + A[j]);
 	}
 
 }
--- 010_MergeSort/src/AscendingSort2.java
+++ 010_MergeSort/src/AscendingSort2.java
@@ -1,9 +1,83 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+// 백준 온라인 2751번 / 교재 128페이지
+
+// 문제 : N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+
+// 입력 : 첫째 줄에 수의 개수 N(1 ≤ N ≤ 1,000,000)이 주어진다. 둘째 줄부터 N개의 줄에는 수가 주어진다. 
+//		 이 수는 절댓값이 1,000,000보다 작거나 같은 정수이다. 수는 중복되지 않는다.
+
+// 출력 : 첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.
 
 public class AscendingSort2 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static int[] A;
+	public static int[] tmp;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		A = new int[N];
+		tmp = new int[N];
+		for (int i = 0; i < N; i++) {
+			A[i] = Integer.parseInt(br.readLine());
+		}
+		merge_sort("A", 0, N-1);
+		for (int i = 0; i < N; i++) {
+			bw.write(A[i] + "\n");
+		}
+		bw.flush();
+		bw.close();
 	}
 
+	public static void merge_sort(String str, int s, int e) {
+		if(e - s < 1) return;
+		int m = (s + e) / 2;
+//		System.out.println("===================================== " + str + ". s = " + s + " / m = " + m + " / e = " + e);
+		merge_sort("B", s , m);
+		merge_sort("C", m + 1, e);
+		for (int i = s; i <= e; i++) {
+			tmp[i] = A[i];
+		}
+		int k = s;
+		int index1 = s;
+		int index2 = m + 1;
+		while(index1 <= m && index2 <= e) {
+			if(tmp[index1] > tmp[index2]) {
+				A[k] = tmp[index2];
+//				System.out.print("1. index1("+index1+") <= m("+m+") && index2("+index2+") <= e("+e+") / "+tmp[index1]+" > "+tmp[index2]+"  ->  k = "+k+", A[k] = " + A[k]);
+				k++;
+				index2++;
+//				System.out.println("  결과 ==>  k = " + k + ", index2 = " + index2);
+			} else {
+				A[k] = tmp[index1];
+//				System.out.print("2. index1("+index1+") <= m("+m+") && index2("+index2+") <= e("+e+") / "+tmp[index1]+" <= "+tmp[index2]+"  ->  k = "+k+", A[k] = " + A[k]);
+				k++;
+				index1++;
+//				System.out.println(" ||  결과 ==>  k = " + k + ", index1 = " + index1);
+			}
+		}
+		while(index1 <= m) {
+			A[k] = tmp[index1];
+//			System.out.print("3. index1("+index1+") <= m("+m+")  ->  k = "+k+", A[k] = " + A[k]);
+			k++;
+			index1++;			
+//			System.out.println(" ||  결과 ==>  k = " + k + ", index1 = " + index1);
+		}
+		while(index2 <= e) {
+//			System.out.print("4. index2("+index2+") <= e("+e+")  ->  k = "+k+", A[k] = " + A[k]);
+			A[k] = tmp[index2];
+			k++;
+			index2++;			
+//			System.out.println(" ||  결과 ==>  k = " + k + ", index2 = " + index2);
+		}
+//		for (int i = 0; i < A.length; i++) {
+//			System.out.print(A[i] + " | ");
+//		}
+//		System.out.println();
+	}
 }
